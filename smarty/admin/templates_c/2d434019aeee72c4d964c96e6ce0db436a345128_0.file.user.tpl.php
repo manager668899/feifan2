ac49086bafe859442877b5f8cc7bfc71b739aefe
manager668899 <362088718@qq.com>
2017-09-15 23:49:01 +0800

    'dddddddddddddddddddddddd'

--- smarty/admin/templates_c/2d434019aeee72c4d964c96e6ce0db436a345128_0.file.user.tpl.php
+++ smarty/admin/templates_c/2d434019aeee72c4d964c96e6ce0db436a345128_0.file.user.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2017-09-15 11:45:24
+/* Smarty version 3.1.30, created on 2017-09-15 15:05:04
   from "D:\phpStudy\WWW\feifan2\smarty\admin\web\user.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_59bb4cd45536a2_13023742',
+  'unifunc' => 'content_59bbec209205c9_75878397',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '2d434019aeee72c4d964c96e6ce0db436a345128' => 
     array (
       0 => 'D:\\phpStudy\\WWW\\feifan2\\smarty\\admin\\web\\user.tpl',
-      1 => 1505445590,
+      1 => 1505487900,
       2 => 'file',
     ),
   ),
@@ -21,18 +21,18 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:base/base.tpl' => 1,
   ),
 ),false)) {
-function content_59bb4cd45536a2_13023742 (Smarty_Internal_Template $_smarty_tpl) {
+function content_59bbec209205c9_75878397 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_loadInheritance();
 $_smarty_tpl->inheritance->init($_smarty_tpl, true);
 ?>
 
 <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_2500759bb4cd45536a1_24326617', 'content');
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_2521359bbec209205c0_19815959', 'content');
 $_smarty_tpl->inheritance->endChild();
 $_smarty_tpl->_subTemplateRender("file:base/base.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 2, false);
 }
 /* {block 'content'} */
-class Block_2500759bb4cd45536a1_24326617 extends Smarty_Internal_Block
+class Block_2521359bbec209205c0_19815959 extends Smarty_Internal_Block
 {
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
 ?>
@@ -67,7 +67,7 @@ $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
 	<nav aria-label="Page navigation">
 
 			  <ul class="pagination">
-			  <li><a href="">每页显示<?php echo $_smarty_tpl->tpl_vars['showpage']->value;?>
+			  <li><a href="">每页显示<?php echo $_smarty_tpl->tpl_vars['showlist']->value;?>
 条</a></li>
 			    <li>
 			      <a href="user.php?page=<?php echo $_smarty_tpl->tpl_vars['page']->value-1;?>
@@ -76,22 +76,50 @@ $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl);
 			      </a>
 			    </li>
 			    <li><a href="user.php?page=1">首页</a></li>
-			        <?php if ($_smarty_tpl->tpl_vars['page']->value > 3) {?><li><a href="">...</a></li><?php }?>
+			        <?php if ($_smarty_tpl->tpl_vars['n']->value > $_smarty_tpl->tpl_vars['showpage']->value) {?>
+			            <?php if ($_smarty_tpl->tpl_vars['page']->value > $_smarty_tpl->tpl_vars['pageoffset']->value+1) {?><li><a href="user.php">...</a></li><?php }?>
+			            <?php if ($_smarty_tpl->tpl_vars['page']->value > $_smarty_tpl->tpl_vars['pageoffset']->value) {?>
+			              <?php $_smarty_tpl->_assignInScope('start', $_smarty_tpl->tpl_vars['page']->value-$_smarty_tpl->tpl_vars['pageoffset']->value);
+?>
+			              <?php if ($_smarty_tpl->tpl_vars['n']->value > $_smarty_tpl->tpl_vars['page']->value+$_smarty_tpl->tpl_vars['pageoffset']->value) {?>
+			                   <?php $_smarty_tpl->_assignInScope('end', $_smarty_tpl->tpl_vars['page']->value+$_smarty_tpl->tpl_vars['pageoffset']->value);
+?>
+			                <?php } else { ?>
+			                   <?php $_smarty_tpl->_assignInScope('end', $_smarty_tpl->tpl_vars['n']->value);
+?>
+			                <?php }?>
+			            <?php }?>
+			            <?php } else { ?>
+	                          <?php $_smarty_tpl->_assignInScope('star', 1);
+?>
+	                       <?php if ($_smarty_tpl->tpl_vars['n']->value > $_smarty_tpl->tpl_vars['showpage']->value) {?>
+	                          <?php $_smarty_tpl->_assignInScope('end', $_smarty_tpl->tpl_vars['showpage']->value);
+?>
+	                       <?php } else { ?>
+	                         <?php $_smarty_tpl->_assignInScope('end', $_smarty_tpl->tpl_vars['n']->value);
+?>
+	                       <?php }?> 
 
-	                 <?php
-$_smarty_tpl->tpl_vars['uu'] = new Smarty_Variable(null, $_smarty_tpl->isRenderingCache);$_smarty_tpl->tpl_vars['uu']->step = 1;$_smarty_tpl->tpl_vars['uu']->total = (int) ceil(($_smarty_tpl->tpl_vars['uu']->step > 0 ? 5+1 - (1) : 1-(5)+1)/abs($_smarty_tpl->tpl_vars['uu']->step));
-if ($_smarty_tpl->tpl_vars['uu']->total > 0) {
-for ($_smarty_tpl->tpl_vars['uu']->value = 1, $_smarty_tpl->tpl_vars['uu']->iteration = 1;$_smarty_tpl->tpl_vars['uu']->iteration <= $_smarty_tpl->tpl_vars['uu']->total;$_smarty_tpl->tpl_vars['uu']->value += $_smarty_tpl->tpl_vars['uu']->step, $_smarty_tpl->tpl_vars['uu']->iteration++) {
-$_smarty_tpl->tpl_vars['uu']->first = $_smarty_tpl->tpl_vars['uu']->iteration == 1;$_smarty_tpl->tpl_vars['uu']->last = $_smarty_tpl->tpl_vars['uu']->iteration == $_smarty_tpl->tpl_vars['uu']->total;?> 
-					    <li><a href="user.php?page=<?php echo $_smarty_tpl->tpl_vars['uu']->value;?>
-"><?php echo $_smarty_tpl->tpl_vars['uu']->value;?>
+	                       <?php if ($_smarty_tpl->tpl_vars['page']->value+$_smarty_tpl->tpl_vars['pageoffset']->value > $_smarty_tpl->tpl_vars['n']->value) {?>
+	                          <?php $_smarty_tpl->_assignInScope('start', $_smarty_tpl->tpl_vars['start']->value-($_smarty_tpl->tpl_vars['page']->value+$_smarty_tpl->tpl_vars['pageoffset']->value-$_smarty_tpl->tpl_vars['end']->value));
+?>
+	                       <?php }?>
+
+			        <?php }?>
+
+                <?php
+$_smarty_tpl->tpl_vars['foo'] = new Smarty_Variable(null, $_smarty_tpl->isRenderingCache);$_smarty_tpl->tpl_vars['foo']->step = 1;$_smarty_tpl->tpl_vars['foo']->total = (int) ceil(($_smarty_tpl->tpl_vars['foo']->step > 0 ? $_smarty_tpl->tpl_vars['end']->value+1 - ($_smarty_tpl->tpl_vars['start']->value) : $_smarty_tpl->tpl_vars['start']->value-($_smarty_tpl->tpl_vars['end']->value)+1)/abs($_smarty_tpl->tpl_vars['foo']->step));
+if ($_smarty_tpl->tpl_vars['foo']->total > 0) {
+for ($_smarty_tpl->tpl_vars['foo']->value = $_smarty_tpl->tpl_vars['start']->value, $_smarty_tpl->tpl_vars['foo']->iteration = 1;$_smarty_tpl->tpl_vars['foo']->iteration <= $_smarty_tpl->tpl_vars['foo']->total;$_smarty_tpl->tpl_vars['foo']->value += $_smarty_tpl->tpl_vars['foo']->step, $_smarty_tpl->tpl_vars['foo']->iteration++) {
+$_smarty_tpl->tpl_vars['foo']->first = $_smarty_tpl->tpl_vars['foo']->iteration == 1;$_smarty_tpl->tpl_vars['foo']->last = $_smarty_tpl->tpl_vars['foo']->iteration == $_smarty_tpl->tpl_vars['foo']->total;?>   
+                 <li><a href="user.php?page=<?php echo $_smarty_tpl->tpl_vars['foo']->value;?>
+"><?php echo $_smarty_tpl->tpl_vars['foo']->value;?>
 </a></li>
-					 <?php }
+                <?php }
 }
 ?>
 
-
-			       <?php if ($_smarty_tpl->tpl_vars['page']->value < ($_smarty_tpl->tpl_vars['n']->value-$_smarty_tpl->tpl_vars['page']->value)) {?><li><a href="">...</a></li><?php }?>
+			        <?php if ($_smarty_tpl->tpl_vars['n']->value > $_smarty_tpl->tpl_vars['showpage']->value && $_smarty_tpl->tpl_vars['n']->value > $_smarty_tpl->tpl_vars['page']->value+$_smarty_tpl->tpl_vars['pageoffset']->value) {?><li><a href="user.php">...</a></li><?php }?>
 			    <li><a href="user.php?page=<?php echo $_smarty_tpl->tpl_vars['n']->value;?>
 ">尾页</a></li>
 			    <li>
@@ -100,6 +128,8 @@ $_smarty_tpl->tpl_vars['uu']->first = $_smarty_tpl->tpl_vars['uu']->iteration ==
 			        <span aria-hidden="true">&raquo;下一页</span>
 			      </a>
 			    </li>
+			     <li><a href="user.php">第<?php echo $_smarty_tpl->tpl_vars['page']->value;?>
+页</a></li>
 			  </ul>
 	</nav>
 <?php
--- smarty/admin/user.php
+++ smarty/admin/user.php
@@ -17,8 +17,8 @@ $count=DB::getDB()->login($sql2);
 
 // echo '<hr/>';
 // echo '总页数';
-$showPage=5;
- $n=ceil($count['id']/$showPage) ;  //总页数
+$showlist=5;//页码每页显示几条数据
+ $n=ceil($count['id']/$showlist) ;  //总页数
 
 if(isset($_GET['page'])){
     $page=$_GET['page'];//第几页
@@ -30,19 +30,27 @@ if(isset($_GET['page'])){
        }
 }
 
-$s=($page-1)*$showPage;
+$s=($page-1)*$showlist;
 // echo '<hr/>';
-// echo $page;
-$sql="select * from study1 limit {$s},{$showPage}"; //5/
+$showpage=5; //显示的页面是5个格子
+// 计算出偏移量
+$pageoffset=($showpage-1)/2;
+
+$sql="select * from study1 limit {$s},{$showlist}"; //5/
 // echo $sql;
-//偏移量
-$pageoffset=($page-1)/2;
-$show=5;
+
 $start=1;
 $end=$n;
+
+
+
 $study1=DB::getDB()->select($sql);
 $smarty->assign('arr',$study1);
 $smarty->assign('page',$page);
+$smarty->assign('start',$start);
 $smarty->assign('n',$n);
-$smarty->assign('showpage',$showPage);
+$smarty->assign('end',$end);
+$smarty->assign('showpage',$showpage);
+$smarty->assign('pageoffset',$pageoffset);
+$smarty->assign('showlist',$showlist);
 $smarty->display('user.tpl');
