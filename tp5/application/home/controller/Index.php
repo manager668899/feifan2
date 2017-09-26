dde1a9ebd9603a65cbb43664e899153466112a12
manager668899 <362088718@qq.com>
2017-09-26 18:00:12 +0800

    'jsjfsjdf'

--- tp5/application/home/controller/Index.php
+++ tp5/application/home/controller/Index.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\home\controller;
 use think\Db;
+use think\Request;
 class Index
 {
     public function index()
@@ -22,5 +23,44 @@ class Index
         echo $ss;
     }
 
-    
+    public function demo3(Request $requset)
+    {
+         $all=$requset->param();//接收请求
+         var_dump($_GET);//等同于$requset->param()
+         echo '<hr/>';
+         var_dump($all);
+    }
+    public function demo4()
+    {
+    	$obj=Request::instance(); //等同于demo3方法中的参数 Request $requset
+    	$all=$obj->param();
+    	var_dump($all);
+    	echo '<hr/>';
+    	var_dump($obj->isGet());
+    	echo '<hr/>';
+    	dump($obj->header());
+    }
+    public function demo5()
+    {
+    	 $all=input('param.');
+    	 var_dump($all);
+    }
+    public function demo6()
+    {
+       // $arr=Db::query('select * from study1');
+       //$bool=Db::execute("insert into study1 (name,age,class)values('Dddddddddd',5,'yyyyyyyy')");
+       //$bool=Db::execute("update study1 set name='我的名字',age=23,class='php班级'where id=97");
+
+       //dump($bool);
+       //$arr=Db::table('study1')->where(['id'=>90])->find();
+        //$arr=Db::table('study1')->where('age','>',20)->select();
+        //$arr=Db::table('tg_style')->where(['class_id'=>1])->select();
+
+        $data=['name'=>'张三','age'=>18,'relish'=>'音乐','class'=>'好好学习'];
+        //$arr=Db::table('study1')->insert($data);
+        $arr=Db::table('study1')->where('id', 1)->update($data);
+        dump($arr);
+    }
+
+
 }
--- tp5/application/route.php
+++ tp5/application/route.php
@@ -10,12 +10,15 @@
 // +----------------------------------------------------------------------
 
 use think\Route;
-Route::get('aaa','home/Index/index');
+Route::get('aaa','home/Index/index');//aaa相当于自定义的名字 'home/Index/index' 实际的路径
 Route::get('aaa2','home/Index/index2');
 Route::get('demo1/[:id]','home/Index/demo1');//可选
 Route::get('demo1a/:id','home/Index/demo1');//必选参数
 Route::get('demo2/[:ss]','home/Index/demo2');//可选参数
-
+Route::get('demo3/[:ss]','home/Index/demo3');//可选参数
+Route::get('demo4/[:ss]','home/Index/demo4');//可选参数
+Route::get('demo5/[:ss]','home/Index/demo5');//可选参数
+Route::get('demo6','home/Index/demo6');//可选参数
 return [
     '__pattern__' => [
         'name' => '\w+',
