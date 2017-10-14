036948612827dc3e639ce52ca10192efd6381b96
manager668899 <362088718@qq.com>
2017-10-14 14:52:49 +0800

    'uuuuu'

---
+++ blog55/app/Http/Controllers/User.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class User extends Controller
+{
+    public function aaa()
+    {
+    	echo 'aaaaaaaaaa';
+    }
+    public function bbb()
+    {
+    	echo 'bbbbbbbbb';
+    }
+}
---
+++ blog55/routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+    
+});
+
+Route::get('foo', function () {
+    //return 'Hello World';
+    return redirect()->route('profile');
+});
+
+Route::get('foo1', function () {
+    return 'Hello Worldttttt';
+});
+Route::redirect('/', '/foo', 301); //路由重定向
+
+Route::get('user/{id?}', function ($id='8888') {
+    return 'User '.$id;
+});
+
+Route::get('usera/profile', function () {
+    echo 'ppppppp';
+})->name('profile');
+
+Route::get('userb/profile', 'UserController@showProfile')->name('profilea');//路由跳转到控制器
+Route::get('aaa', 'User@aaa');
+Route::get('bbb', 'User@bbb');
+
+
